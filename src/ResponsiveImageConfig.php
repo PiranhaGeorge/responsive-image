c6f565d914bc144a7206725c1e54bc5615c378d6
brendt <brent.roose@gmail.com>
2026-05-29 10:41:51 +0200

    wip

--- src/ResponsiveImageConfig.php
+++ src/ResponsiveImageConfig.php
@@ -8,10 +8,15 @@ use Intervention\Image\ImageManager;
 final class ResponsiveImageConfig
 {
     public function __construct(
+        /** @param string $srcPath The path to the directory where all source images are stored. */
         public string $srcPath,
+        /** @param string $publicPath The path to the public directory where rendered images should be served from. */
         public string $publicPath,
+        /** @param bool $async Whether responsive images should be generated in the background. This paramater is only taken into account if tempest/command-bus is installed. */
         public bool $async = true,
+        /** @param bool $cache Whether generated responsive variants should be cached. If true, then responsive variants won't be generated as long as the main image file exists in the public path. Cache clearing should be done manually on your end. */
         public bool $cache = true,
+        /** @param ImageManager $imageManager The Intervention imagemanager. Refer to the [Intervention docs](https://image.intervention.io/v4) for all options. */
         public ImageManager $imageManager = new ImageManager(new Driver()),
     ) {
         $this->srcPath = realpath($srcPath) ?: $this->srcPath;
